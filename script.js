55db7b6fa23cd0a91e993c9a35f570eedbcff55a
SilverWings47 <nasrisami17@gmail.com>
2022-09-29 22:36:21 +0100

    Created a mobile responsive navbar

---
+++ script.js
@@ -0,0 +1,20 @@
+const productBtn = document.querySelector('.product-btn');
+const productSubMenu = document.querySelectorAll('.sub-menu')[0];
+
+const cartBtn = document.querySelector('.cart-btn');
+const cartSubMenu = document.querySelectorAll('.sub-menu')[1];
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+productBtn.addEventListener('click', () => {
+  productSubMenu.classList.toggle('sub-menu-active');
+});
+
+cartBtn.addEventListener('click', () => {
+  cartSubMenu.classList.toggle('sub-menu-active');
+});
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu-active');
+});
